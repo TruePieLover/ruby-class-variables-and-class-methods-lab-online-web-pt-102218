4295f9ea9ad62850f5bb1f0e5ae7d59d9ff7b5ed
Brandon Teich <brandonteich123@gmail.com>
2018-11-16 17:56:05 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,4 +3,11 @@ class Song
   @@count = 0
   @@artists = []
   @@genres = []
-  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
